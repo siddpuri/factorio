2a1ea59a963d4f5176864e4556796af16a0c64dc
Sidd Puri <sidd@studiopuri.com>
2024-12-17 07:03:26 +0000

    First working version

--- factorio.py
+++ factorio.py
@@ -1,41 +1,116 @@
-from typing import Tuple, List, Dict
+from typing import Tuple, List, Dict, Set
 
 db_file_name = "recipes.txt"
+
+speeds = {
+    "Raw": 1,
+    "Furnace": 2,
+    "Factory": 0.75,
+    "Chemical plant": 1,
+}
+
+rate = 0.6
 
 class Item:
     name: str
     amount: int
     time: float
-    ingredients: List[Tuple[str, int]]
+    sequence: int
+    ingredients: List[Tuple["Item", int]]
 
-    def __init__(self, name: str, amount: int, time: float):
-        assert amount > 0
-        assert time > 0
+    def __init__(self, name: str, amount: int, time: float, sequence: int):
         self.name = name
         self.amount = amount
         self.time = time
+        self.sequence = sequence
         self.ingredients = []
 
     def __str__(self):
         return self.name
 
-    def add(self, ingredient: Tuple[str, int]):
-        self.ingredients.append(ingredient)
+    def add(self, ingredient: "Item", amount: int):
+        self.ingredients.append((ingredient, amount))
 
-class Db:
+class Catalog:
     items: Dict[str, Item]
 
     def __init__(self):
         self.items = {}
+        self.read_recipes()
+
+    def read_recipes(self) -> None:
+        speed = 1
         item = None
         with open(db_file_name) as f:
             for line in f:
-                if not line.startswith(' '):
-                    name, amount, time = line.split(", ")
-                    assert self.items.get(name) is None
-                    item = Item(name.strip(), int(amount), float(time))
+                indented = line.startswith(" ")
+                line = line.strip()
+                if not line:
+                    pass
+                elif line.startswith("["):
+                    speed = speeds[line[1:-1]]
+                elif not indented:
+                    if ", " in line:
+                        name, amount, time = line.split(",")
+                    else:
+                        name, amount, time = line, 1, 1
+                    assert name not in self.items
+                    item = Item(name, int(amount), float(time) / speed, len(self.items))
                     self.items[name] = item
                 else:
-                    assert item is not None
                     name, amount = line.split(", ")
-                    item.add(name.strip(), int(amount))
+                    item.add(self.items[name], int(amount))
+
+class ShoppingList:
+    items: List[Item]
+    rates: List[float]
+
+    def __init__(self, item: Item, rate: float):
+        self.items = []
+        self.add_ingredients(item)
+        self.items.sort(key=lambda x: x.sequence, reverse=True)
+        self.calculate_rates(rate)
+
+    def add_ingredients(self, item: Item) -> None:
+        if item in self.items:
+            return
+        self.items.append(item)
+        for ingredient, _ in item.ingredients:
+            self.add_ingredients(ingredient)
+
+    def calculate_rates(self, rate) -> None:
+        self.rates = [0] * len(self.items)
+        self.rates[0] = rate
+        for i in range(len(self.items)):
+            for ingredient, amount in self.items[i].ingredients:
+                j = self.items.index(ingredient)
+                assert j > i
+                self.rates[j] += self.rates[i] / self.items[i].amount * amount
+
+    def print(self) -> None:
+        for i in range(len(self.items)):
+            if not self.items[i].ingredients:
+                continue
+            item = self.items[i]
+            rate = self.rates[i]
+            print(f"{item.name:16}: {rate:4.1f} {rate * item.time / item.amount:4.1f}")
+        print()
+
+class Toplevel:
+    db: Catalog
+
+    def __init__(self):
+        self.db = Catalog()
+
+    def run(self) -> None:
+        while True:
+            name = input("Enter item name: ")
+            if not name:
+                return
+            item = self.db.items.get(name)
+            if item is None:
+                print(f"Item {name} not found")
+            else:
+                ShoppingList(item, rate).print()
+
+Toplevel().run()
